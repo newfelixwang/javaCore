ac6e52057d77a950f8c2a3e904dc2ae8b83aa532
flc <1299428902@qq.com>
2023-02-24 09:38:49 +0800

    提交pom中阿里云配置

--- felix-shop/src/main/java/com/wang/felixshop/FelixShopApplication.java
+++ felix-shop/src/main/java/com/wang/felixshop/FelixShopApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FelixShopApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("修改");
         SpringApplication.run(FelixShopApplication.class, args);
     }
 
